36db5ba01f1749000b717cd76d933d261d703d01
boyiZheng99 <162138080+boyiZheng99@users.noreply.github.com>
2025-05-05 22:40:57 +0300

    Update helper.py

--- utils/helper.py
+++ utils/helper.py
@@ -3,7 +3,7 @@ import numpy as np
 
 def get_word_file():
 
-    WordFilePath = '../retinal_dataset/word_embbeding/retinal_word_embedding_300_expert_knowledge_DR5.npy'      
+    WordFilePath = '../retinal_word_embedding_300_expert_knowledge_DR5.npy'      
     WordFile = np.load(WordFilePath)
 
     return WordFile
